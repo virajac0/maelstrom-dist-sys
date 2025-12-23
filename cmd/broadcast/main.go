96424c834c424bdfb2b5d90acbd2d93c67780879
virajac0 <viraj.ac29@gmail.com>
2025-12-23 17:11:16 -0500

    3a: single node broadcast

---
+++ cmd/broadcast/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	var messages []int
+	var topology map[string]any
+	_ = topology
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		messages = append(messages, int(body["message"].(float64)))
+
+		return n.Reply(msg, map[string]string{"type": "broadcast_ok"})
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		return n.Reply(msg, map[string]any{"type": "read_ok", "messages": messages})
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology = body["topology"].(map[string]any)
+
+		return n.Reply(msg, map[string]string{"type": "topology_ok"})
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}

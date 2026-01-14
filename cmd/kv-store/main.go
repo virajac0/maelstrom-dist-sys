ea76d1fd40d550d79433cbf64f49729ede8bc3ec
virajac0 <viraj.ac29@gmail.com>
2026-01-13 23:40:38 -0500

    6b: totally-available, read uncommitted transactions

--- cmd/kv-store/main.go
+++ cmd/kv-store/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -13,6 +14,49 @@ func main() {
 
 	var mu sync.RWMutex
 	kv := make(map[any]any)
+
+	go func() {
+		for {
+			time.Sleep(500 * time.Millisecond)
+
+			mu.RLock()
+			snapshot := make(map[any]any)
+			for k, v := range kv {
+				snapshot[k] = v
+			}
+			mu.RUnlock()
+
+			for _, neighbor := range n.NodeIDs() {
+				if neighbor == n.ID() {
+					continue
+				}
+				n.Send(neighbor, map[string]any{
+					"type": "gossip",
+					"kv":   snapshot,
+				})
+			}
+		}
+	}()
+
+	n.Handle("gossip", func(msg maelstrom.Message) error {
+		var body struct {
+			KV map[string]any `json:"kv"`
+		}
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		mu.Lock()
+		for k, v := range body.KV {
+			// Because writes are unique, we can just fill in what we are missing.
+			// If we already have the key, we don't need to do anything since it won't change.
+			if _, exists := kv[k]; !exists {
+				kv[k] = v
+			}
+		}
+		mu.Unlock()
+		return nil
+	})
 
 	n.Handle("txn", func(msg maelstrom.Message) error {
 		var body map[string]any
